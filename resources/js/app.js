45817fc404db2a2de9b813ad9c343eaeeda116d4
Mandeep Singh <mesingh90@gmail.com>
2019-06-03 21:54:56 +0530

    some cleanup and moved sitemap to local setup
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,5 @@
-// import Highlight from './modules/highlight';
-// import hljs from 'highlight.js/lib/highlight';
-// import 'highlight.js/styles/github.css';
-
 
 import Prism from 'prismjs'
-// import 'prismjs/themes/prism.css'
 import 'prismjs/components/prism-bash.min.js'
 
 
@@ -13,22 +8,7 @@ import Turbolinks from 'turbolinks';
 Turbolinks.start();
 
 document.addEventListener('turbolinks:load', () => {
-    // Highlight.start();
 
     Prism.highlightAll();
-
-    // hljs.configure({ useBR: true });
-
-    // document.querySelectorAll('pre code').forEach((block) => {
-    //     hljs.highlightBlock(block);
-    // });
-
-    // hljs.initHighlightingOnLoad();
-    // document.querySelectorAll('pre code').forEach((block) => {
-    //     console.log(block)
-    //     hljs.highlightBlock(block);
-
-    //     console.log(hljs)
-    // });
 
 });
