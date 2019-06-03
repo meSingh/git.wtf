18bab2ef6f95c7e5283e69a1c13f3be2b70fe031
Mandeep Singh <mesingh90@gmail.com>
2019-06-03 20:25:06 +0530

    changeed tag url for back support
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ Route::feeds();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('tags/{slug}', 'HomeController@tag')->name('tag');
+Route::get('tag/{slug}', 'HomeController@tag')->name('tag');
 Route::get('category/{slug}', 'HomeController@category')->name('category');
 Route::get('{slug}', 'HomeController@show')->name('post');
