2da10090a093f61eb15374cfab7299d62124b861
Mandeep Singh <mesingh90@gmail.com>
2019-06-04 19:18:29 +0530

    updated sitemap setup
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

--- app/Console/Commands/SitemapGenerateCommand.php
+++ app/Console/Commands/SitemapGenerateCommand.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Sitemap\SitemapGenerator;
+// use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+use App\Content\Posts;
+use Carbon\Carbon;
 
 class SitemapGenerateCommand extends Command
 {
@@ -26,12 +30,42 @@ class SitemapGenerateCommand extends Command
      *
      * @return mixed
      */
-    public function handle()
+    public function handle(Posts $posts)
     {
-        // dd(config('app.url'));
-        // modify this to your own needs
         // SitemapGenerator::create(config('app.url'))
-        SitemapGenerator::create('https://git.wtf')
-            ->writeToFile(public_path('sitemap.xml'));
+        $sitemap = Sitemap::create()
+            ->add(Url::create('/')
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(0.9));
+
+        $tags = $categories = [];
+
+        foreach ($posts->all() as $post) {
+            $sitemap->add(Url::create($post->url)
+                ->setLastModificationDate($post->updated)
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
+                ->setPriority(0.8));
+
+            $tags = array_merge($post->tags, $tags);
+            $categories[] = $post->category;
+        }
+
+        foreach ($categories as $category) {
+            $sitemap->add(Url::create(route('category', $category))
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.6));
+        }
+
+        foreach ($tags as $tag) {
+            $sitemap->add(Url::create(route('tag', $tag))
+                ->setLastModificationDate(Carbon::yesterday())
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(0.4));
+        }
+
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
     }
 }
