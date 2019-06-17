922e280f98f543191b3c6a60d25552df87a8944a
Mandeep Singh <mesingh90@gmail.com>
2019-06-17 19:22:42 +0530

    various design updates
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,7 @@
 
             <p class="mb-8">So here are several of those tricky situations that you might find yourself in with <i>simple ways to get out peacefully.</i></p>
             <div class="flex justify-center">
-                <a href="{{ route('post', $first->slug) }}" class="bg-indigo-500 hover:bg-indigo-700 w-full md:w-1/3 text-white py-3 font-semibold rounded inline-block text-center hover:text-white no-underline border-none text-base cursor-pointer cta-button-home">Get solutions to your GIT problems!</a>
+                <a href="{{ route('post', $first->slug) }}" class="bg-indigo-500 hover:bg-indigo-700 w-full md:w-1/3 text-white py-3 font-semibold rounded inline-block text-center hover:text-white no-underline border-none text-base cursor-pointer cta-button-home shadow-md">Get solutions to your GIT problems!</a>
             </div>
         </div>
     </section>
@@ -37,7 +37,7 @@
 
 
             @if( $loop->index == 4)
-                <section class="bg-gray-200 shadow-theme my-6 max-w-5xl rounded py-3 px-6">
+                <section class="bg-gray-200 my-6 max-w-5xl shadow-inner md:shadow md:rounded py-3 px-6">
                     <h3 class="mb-0">Want to Contribute?</h3>
 
                     <p class="mb-4">Send me an <a href="mailto:{{ config('me.email') }}?Subject={{ config('me.name') }}">Email</a>, ping me on <a href="https://twitter.com/{{ config('me.social.twitter') }}" target="_blank" rel="noopener" class="text-blue-500 hover:text-blue-300">Twitter</a> or submit a pull request on <a href="https://github.com/{{ config('me.social.github') }}/pulls" target="_blank" rel="noopener" class="text-orange-400 hover:text-orange-300">Github</a>.</p>
@@ -45,7 +45,7 @@
                 </section>
             @endif
         @empty
-            <section class="bg-white shadow-theme xl:rounded mb-8 p-8 text-center">
+            <section class="bg-white shadow-md xl:rounded mb-8 p-8 text-center">
                 <h3 class="font-normal">No articles found!!</h3>
                 <a href="{{ route('home') }}" class="text-sm text-purple-600 hover:text-purple-600 border-2 border-purple-500 py-1 px-2 font-semibold rounded mt-4 inline-block no-underline">« Go Home</a>
             </section>
