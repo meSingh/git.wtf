b79784547008e3777e150c9ea8038ed79e47cf9d
Mandeep Singh <mesingh90@gmail.com>
2019-07-17 14:07:16 +0530

    fixed broken links
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('tag/{slug}', 'HomeController@tag')->name('tag');
 Route::get('category/{slug}', 'HomeController@category')->name('category');
-Route::get('{slug}', 'HomeController@show')->name('post');
 
 Route::permanentRedirect(
     '/how-to-change-the-message-on-last-commit',
@@ -18,3 +17,20 @@ Route::permanentRedirect(
     '/how-to-move-accidentally-committed-changes-to-a-brand-new-branch',
     '/how-to-move-accidentally-committed-changes-to-a-brand-new-branch-in-git'
 );
+
+Route::permanentRedirect(
+    '/how-to-sync-my-commits-with-multiple-repositories',
+    '/how-to-sync-my-commits-with-multiple-repositories-in-git'
+);
+
+Route::permanentRedirect(
+    '/how-to-squash-last-x-commits-together',
+    '/how-to-squash-last-x-commits-together-in-git'
+);
+
+Route::permanentRedirect(
+    '/how-to-revert-last-x-commits',
+    '/how-to-revert-last-x-commits-in-git'
+);
+
+Route::get('{slug}', 'HomeController@show')->name('post');
