ccca4161742ebb643a8d1ed8952462836430b870
Mandeep Singh <mesingh90@gmail.com>
2019-07-24 16:58:00 +0530

    added howuku code
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
+
 <head>
     @include('layouts.partials.analytics')
     @include('layouts.partials.head')
@@ -8,10 +9,10 @@
 
 </head>
 <!-- <body class="font-brand text-base tracking-normal align-middle leading-relaxed text-gray-900 m-0 relative bg-gray-200 " style="background: #364f6b"> -->
+
 <body class="font-sans text-base tracking-wide leading-relaxed text-brand-alt m-0 relative bg-gray-400">
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.analytics.gtm_id') }}"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ config('services.analytics.gtm_id') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
 
@@ -22,7 +23,7 @@
                 <span class="w-12 mr-1 text-brand">@include('layouts/partials/logo')</span>
             </a>
 
-            <form action="/" method="get"  class="w-full px-12 pt-4 mr-2 flex hidden md:block">
+            <form action="/" method="get" class="w-full px-12 pt-4 mr-2 flex hidden md:block">
                 <input value="{{ request()->get('query') }}" name="query" class="transition text-base focus:outline-none border border-gray-300 border-solid focus:bg-white focus:border-gray-300 placeholder-gray-900 rounded bg-gray-200 py-3 pr-4 px-6 w-full appearance-none" type="text" placeholder="Search for git tips here" autocomplete="off" spellcheck="false">
             </form>
 
@@ -32,12 +33,12 @@
                 <span class="font-semibold">Submit Article</span>
             </a> -->
             <div class="flex">
-            <a href="https://github.com/{{ config('me.social.github') }}" target="_blank" rel="noopener" data-turbolinks="false" class="pl-2 sm:pl-2 sm:pr-2 py-4 md:py-6 text-gray-700 mr-2 md:mr-0">{{ svg('github') }}</a>
-            <a href="https://twitter.com/{{ config('me.social.twitter') }}" target="_blank" rel="noopener" data-turbolinks="false" class="pl-2 sm:pl-2 sm:pr-2 py-4 md:py-6 text-gray-700">{{ svg('twitter') }}</a>
+                <a href="https://github.com/{{ config('me.social.github') }}" target="_blank" rel="noopener" data-turbolinks="false" class="pl-2 sm:pl-2 sm:pr-2 py-4 md:py-6 text-gray-700 mr-2 md:mr-0">{{ svg('github') }}</a>
+                <a href="https://twitter.com/{{ config('me.social.twitter') }}" target="_blank" rel="noopener" data-turbolinks="false" class="pl-2 sm:pl-2 sm:pr-2 py-4 md:py-6 text-gray-700">{{ svg('twitter') }}</a>
             </div>
         </div>
         <div class="">
-            <form action="/" method="get"  class="w-full px-4 pt-2 pb-8 flex md:hidden">
+            <form action="/" method="get" class="w-full px-4 pt-2 pb-8 flex md:hidden">
                 <input value="{{ request()->get('query') }}" name="query" class="transition text-base focus:outline-none border border-gray-300 border-solid focus:bg-white focus:border-gray-300 placeholder-gray-900 rounded bg-gray-200 py-2 pr-4 px-6 w-full appearance-none" type="text" placeholder="Search for git tips here" autocomplete="off" spellcheck="false">
             </form>
         </div>
@@ -52,5 +53,7 @@
     @include('layouts.partials.footer')
 
 
+    <script type="text/javascript" src="https://cdn.howuku.com/js/howu.js" key="N6V9xer17B7rVROvGQJ53g"></script>
 </body>
+
 </html>
