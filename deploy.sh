174703512693fd4d427b1557392c6a4b0c09e2ed
Mandeep Singh <mesingh90@gmail.com>
2020-02-26 19:28:18 +0530

    added deployment script
    Signed-off-by: Mandeep Singh <mesingh90@gmail.com>

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# This script is supposed to be run after pulling all
+# the changes from git remote.
+
+cd /home/msingh/git.wtf
+pwd
+
+# Pull latest code from github
+git pull origin master
+
+# Let's tie any loose end's if there are any..
+composer install -q --no-ansi --no-interaction --no-suggest --no-progress --prefer-dist --optimize-autoloader --no-dev
+
+# If everything worked out correctly then clear any previous caches and rebuild new ones
+php artisan cache:clear
+php artisan config:cache
+php artisan route:cache
+php artisan responsecache:clear
+php artisan algolia:index
+
+# Finally, compile any assets that might have been updated
+yarn install
+yarn run production
+
+php artisan sitemap:generate
